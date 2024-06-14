063f0bcc2e807e6cf1729bef5f68f694461f4b8c
randomstr1ng <julian@petersohn.it>
2024-06-14 13:41:55 +0200

    update nsg

--- terraform/kalivm.tf
+++ terraform/kalivm.tf
@@ -53,13 +53,13 @@ resource "azurerm_network_security_group" "kali-nsg" {
     destination_address_prefix = "*"
   } 
   security_rule {
-    name                       = "allow-https-inbound"
+    name                       = "allow-alt-https-inbound"
     priority                   = 102
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "443"
+    destination_port_range     = "8443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   } 

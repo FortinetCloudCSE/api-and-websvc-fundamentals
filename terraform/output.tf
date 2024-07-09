6228584c5cf8f87d1ae7f6e988eb00bba337cdff
fortidg <dgooch@fortinet.com>
2024-07-09 12:29:57 +0000

    added ubuntu ip to outputs

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,10 @@ output "kali_linux_PublicIP" {
   value       = azurerm_public_ip.kalipip.ip_address
 }
 
+output "ubuntu_PublicIP" {
+  value       = azurerm_public_ip.ubupip.ip_address
+}
+
 output "ssh-username" {
   value       = azurerm_linux_virtual_machine.kalivm.admin_username 
 }
